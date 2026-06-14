541890cfa95b3f7951f58c83cdac48affa11ac4f
najif jawoad jim <najifjawoad@gmail.com>
2026-06-14 23:26:28 +0600

    added query 4 to 7

--- query.sql
+++ query.sql
@@ -75,4 +75,32 @@ where full_name like 'Tanvir%' or full_name ilike '%Haque%' ;
 select booking_id , user_id , match_id ,
   coalesce(payment_status , 'Action Required') as systematic_status
   from bookings 
-where payment_status is null
+where payment_status is null 
+-- Query-4 :
+select
+b.booking_id ,
+u.full_name ,
+m.fixture,
+b.total_cost 
+
+from bookings as b
+inner join users as u on b.user_id = u.user_id 
+inner join matches as m  on b.match_id = m.match_id ;
+
+
+-- Query-5 :
+select 
+  u.user_id,
+  u.full_name,
+  b.booking_id 
+  from users as u
+left join bookings as b on u.user_id = b.user_id
+    
+-- Query-6 :
+select booking_id , match_id , total_cost
+  from bookings
+  where total_cost > (select avg(total_cost) from bookings)
+-- Query-7 :
+select match_id , fixture , base_ticket_price
+from matches order by base_ticket_price desc 
+limit 2 offset 1 ;
